86015da8029850438d1d6492e1e4920ffae18605
DrazyX <drazyxtv@gmail.com>
2023-02-01 16:05:02 +0100

    + Affichage des sprites sur la map 2d et correction du lag quand on se rapproche d'un sprite

--- main.py
+++ main.py
@@ -8,7 +8,17 @@ from interface import *
 from menu import *
 from mouvement import *
 from raycasting import *
-from sprites import Sprite
+from sprites import *
+
+
+def draw_object():
+    for sprite in sprites.values():
+        pg.draw.circle(screen, (0, 120, 200), sprite["position"], 4)
+
+    #  Joueur
+    pg.draw.circle(screen, (255, 0, 0), (player_x, player_y), 4)
+    pg.draw.line(screen, (255, 0, 0), (player_x, player_y),
+                 (player_x - 25 * m.sin(player_rotation), player_y - 25 * m.cos(player_rotation)))
 
 
 def draw_minimap():
@@ -16,13 +26,9 @@ def draw_minimap():
         for x, column in enumerate(line):
             pg.draw.rect(
                 screen,
-                (200, 200, 200) if map["map"][y][x] == 0 else (100 * abs(map["map"][y][x]) / 2, 100, 100),
+                (200, 200, 200) if map["map"][y][x] == 0 else (100, 100, 100),
                 (x * TS, y * TS, TS, TS)
             )
-
-    pg.draw.circle(screen, (255, 0, 0), (player_x, player_y), 4)
-    pg.draw.line(screen, (255, 0, 0), (player_x, player_y),
-                 (player_x - 25 * m.sin(player_rotation), player_y - 25 * m.cos(player_rotation)))
 
 
 Menu()
@@ -44,14 +50,17 @@ while running:
 
     if mouse.get_focused():
         from constantes import speed
-        #speed = speed * (60/clock.get_fps())
-        player_x, player_y, player_rotation, HEIGHT = Deplacements(current_speed, speed, HEIGHT)
+
+        # speed = speed * (60/clock.get_fps())
+        player_x, player_y, player_rotation, HEIGHT = Deplacements(current_speed, speed, HEIGHT, player_x, player_y,
+                                                                   player_rotation)
         mid = (tailleY / 2) + HEIGHT
         pg.draw.rect(screen, (50, 50, 50), (0, 0, tailleX, mid))
         pg.draw.rect(screen, (30, 30, 30), (0, mid, tailleX, tailleY - mid))
         dist_list = RayCasting(player_x, player_y, player_rotation, HEIGHT)
-        Sprite(player_x,player_y,player_rotation,HEIGHT, dist_list)
+        Sprite(player_x, player_y, player_rotation, HEIGHT, dist_list)
         draw_minimap()
+        draw_object()
         screen.blit(Crosshair, Crosshair_coord)
     pg.display.flip()
     clock.tick(60)
--- sprites.py
+++ sprites.py
@@ -8,11 +8,20 @@ sprites = {
         "width": 37,
         "ratio": 37 / 51,
         "position": (50, 50)
+    },
+    "2": {
+        "texture": pg.image.load("Assets/Sprites/Ennemis/test.png"),
+        "height": 51,
+        "width": 37,
+        "ratio": 37 / 51,
+        "position": (120, 120)
     }
 }
 coef_angle_tailleX = tailleX / fov_r
+max_height = tailleY * 1.5
 
-def Sprite_angle(player_x,player_y,sprite_x,sprite_y):
+
+def Sprite_angle(player_x, player_y, sprite_x, sprite_y):
     base_angle = m.atan(abs(sprite_x - player_x) / abs(sprite_y - player_y))
 
     if player_x > sprite_x and player_y > sprite_y:
@@ -28,11 +37,11 @@ def Sprite_angle(player_x,player_y,sprite_x,sprite_y):
         return m.radians(180) + base_angle
 
 
-def Sprite(player_x,player_y,player_rotation, HEIGHT, dist_list):
+def Sprite(player_x, player_y, player_rotation, HEIGHT, dist_list):
     # Fonction pour afficher les sprites
     for sprite in sprites.values():
         sprite_x, sprite_y = sprite["position"]
-        sprite_angle = Sprite_angle(player_x,player_y,sprite_x,sprite_y)
+        sprite_angle = Sprite_angle(player_x, player_y, sprite_x, sprite_y)
         sp_pl_angle = sprite_angle - player_rotation
         fov_plus, fov_moins = HALF_FOV, - HALF_FOV
 
@@ -41,16 +50,12 @@ def Sprite(player_x,player_y,player_rotation, HEIGHT, dist_list):
         if fov_moins <= sp_pl_angle <= fov_plus:
 
             column = int(abs(coef_angle_tailleX * (sp_pl_angle - HALF_FOV)))
-            print(column)
+            # print(column)
 
             sprite_distance = Distance(sprite_x - player_x, sprite_y - player_y)
 
-            if sprite_distance <= dist_list[int(column//LINE_SIZE)] :
-                if sprite_distance > 1:
-                    sprite_height = int(((TS / 1) / sprite_distance) * SCREEN_DISTANCE)
-                else:
-                    sprite_height = tailleY
-
+            if 15 < sprite_distance <= dist_list[int(column // LINE_SIZE)]:
+                sprite_height = int(((TS / 1) / sprite_distance) * SCREEN_DISTANCE)
                 sprite_width = sprite_height * sprite["ratio"]
                 scaled_sprite = pg.transform.scale(sprite["texture"], (sprite_width, sprite_height))
-                screen.blit(scaled_sprite, (column - sprite_width//2, (tailleY / 2) - (sprite_height // 2) + HEIGHT))
+                screen.blit(scaled_sprite, (column - sprite_width // 2, (tailleY / 2) - (sprite_height // 2) + HEIGHT))
