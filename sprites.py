abf046a9db77bdb16a28b09fbab346dd7971a368
Antoine Giraudon <119340904+AntonioRodriguez38@users.noreply.github.com>
2023-03-22 14:18:23 +0100

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -5,45 +5,7 @@ from constantes import *
 from functions import Distance, Verif_Angle
 from Sons import gun_sound
 
-sprites = {
-    "1": {
-        "texture": pg.image.load("Assets/Sprites/Ennemis/test.png"),
-        "height": 51,
-        "width": 37,
-        "ratio": 37 / 51,
-        "position": (120, 120),
-        "class": "ennemy",
-        "HP": 100
-    },
-    "2": {
-        "texture": pg.image.load("Assets/Sprites/Ennemis/test.png"),
-        "height": 51,
-        "width": 37,
-        "ratio": 37 / 51,
-        "position": (210, 210),
-        "class": "ennemy",
-        "HP": 100
-    },
 
-    "3": {
-        "texture": pg.image.load("Assets/Sprites/greenlight.png"),
-        "height": 64,
-        "width": 64,
-        "ratio": 1,
-        "position": (37, 37),
-        "class": "neutral"
-    },
-    
-    "4": {
-        "texture": pg.image.load("Assets/Sprites/greenlight.png"),
-        "height": 64,
-        "width": 64,
-        "ratio": 1,
-        "position": (0,0),
-        "class": "NPC"
-    
-    }
-}
 coef_angle_tailleX = tailleX / fov_r
 max_height = tailleY * 1.5
 
@@ -62,7 +24,7 @@ def Sprite_calcul(player_x, player_y, player_rotation, HEIGHT, dist_list, volume
                     sprite_distance = Distance(sprite_x - player_x, sprite_y - player_y) * m.cos(sp_pl_angle)
 
                     if 15 < sprite_distance <= dist_list[int(column // LINE_SIZE)]:
-                        print(sp_pl_angle)
+                       # print(sp_pl_angle)
                         sprite_height = int(((TS / 1.05) / sprite_distance) * SCREEN_DISTANCE)
                         sprite_width = sprite_height * sprite["ratio"]
                         scaled_sprite = pg.transform.scale(sprite["texture"], (sprite_width, sprite_height))
@@ -72,7 +34,7 @@ def Sprite_calcul(player_x, player_y, player_rotation, HEIGHT, dist_list, volume
 
             else:
                 column = int(abs(coef_angle_tailleX * (sp_pl_angle - HALF_FOV)))
-                # print(column)
+
 
                 sprite_distance = Distance(sprite_x - player_x, sprite_y - player_y) * m.cos(sp_pl_angle)
 
@@ -87,14 +49,14 @@ def Sprite_calcul(player_x, player_y, player_rotation, HEIGHT, dist_list, volume
         left, middle, right = pg.mouse.get_pressed()
         speed, damage = weapons[weapon]["speed"], weapons[weapon]["damage"]
 
+
         if left and t.time() - last_shot >= speed:
 
+            last_shot = t.time()
             gun_sound(weapons[weapon]["sound"], weapons[weapon]["volume"])
 
             if viewed_sprite:
                 attack(sp_pl_angle,damage)
-                last_shot = t.time()
-                print(last_shot)
 
         return last_shot
 
@@ -103,7 +65,7 @@ def Sprite_calcul(player_x, player_y, player_rotation, HEIGHT, dist_list, volume
 def Sprite_angle(player_x, player_y, sprite_x, sprite_y):
     base_angle = m.atan(abs(sprite_x - player_x) / abs(sprite_y - player_y))
 
-    if player_x > sprite_x and player_y > sprite_y:
+    if player_x > sprite_x and player_y > sprite_y  :
         return base_angle
 
     elif player_x > sprite_x and player_y < sprite_y:
@@ -116,7 +78,7 @@ def Sprite_angle(player_x, player_y, sprite_x, sprite_y):
         return m.radians(180) + base_angle
 
 
-def Sprite(player_x, player_y, player_rotation, HEIGHT, dist_list, volume, last_shot, weapon):
+def Sprite(player_x, player_y, player_rotation, HEIGHT, dist_list, volume, last_shot, weapon, sprites):
     global sprite
     # Fonction pour afficher les sprites
     for sprite in sprites.values():
@@ -137,9 +99,10 @@ def Sprite(player_x, player_y, player_rotation, HEIGHT, dist_list, volume, last_
 
 
 
-def draw_object():
+def draw_object(sprites, player_x, player_y, player_rotation):
     for sprite in sprites.values():
-        pg.draw.circle(screen, (0, 120, 200), sprite["position"], 4)
+        if sprite["class"]=="ennemy":
+            pg.draw.circle(screen, (0, 120, 200), sprite["position"], 4)
 
     #  Joueur
     pg.draw.circle(screen, (255, 0, 0), (player_x, player_y), 4)
@@ -150,6 +113,6 @@ def draw_object():
 def attack(sprite_angle, damage):
     global sprite
 
-    if -0.05 <= sprite_angle <= 0.05 and sprite["class"] == "ennemy":
+    if -0.15 <= sprite_angle <= 0.05 and sprite["class"] == "ennemy":
         sprite["HP"] -= damage
         print("hit")
