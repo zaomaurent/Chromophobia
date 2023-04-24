29ccfe1f39df8851955ae83dbe6482bdb9112aa2
fakssy <119786294+fakssy@users.noreply.github.com>
2023-04-24 10:53:39 +0200

    Update puissance_4.py

--- puissance_4.py
+++ puissance_4.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-def f_verifligne(hauteur,longueur,alignement_de_4_pions,couleur,tab):
+def f_verifligne(hauteur,longueur,alignement_de_4_pions,couleur,tab): #fonction verifiant si il y a une ligne de 4 pions alignes en s'aidant de la hauteur du dernier pion qui a ete place
   if tab[hauteur][0]==couleur and tab[hauteur][1]==couleur and tab[hauteur][2]==couleur and tab[hauteur][3]==couleur:
     alignement_de_4_pions=False
   elif tab[hauteur][1]==couleur and tab[hauteur][2]==couleur and tab[hauteur][3]==couleur and tab[hauteur][4]==couleur:
@@ -9,7 +9,8 @@ def f_verifligne(hauteur,longueur,alignement_de_4_pions,couleur,tab):
   elif tab[hauteur][3]==couleur and tab[hauteur][4]==couleur and tab[hauteur][5]==couleur and tab[hauteur][6]==couleur:
     alignement_de_4_pions=False
   return alignement_de_4_pions
-def f_verifcolonne(hauteur,longueur,alignement_de_4_pions,couleur,tab):
+
+def f_verifcolonne(hauteur,longueur,alignement_de_4_pions,couleur,tab):  #fonction verifiant si il y a une colonne de 4 pions alignes en s'aidant de la colonne du dernier pion qui a ete place
   if tab[0][longueur]==couleur and tab[1][longueur]==couleur and tab[2][longueur]==couleur and tab[3][longueur]==couleur :
     alignement_de_4_pions=False
   elif tab[1][longueur]==couleur and tab[2][longueur]==couleur and tab[3][longueur]==couleur and tab[4][longueur]==couleur :
@@ -17,7 +18,8 @@ def f_verifcolonne(hauteur,longueur,alignement_de_4_pions,couleur,tab):
   elif tab[2][longueur]==couleur and tab[3][longueur]==couleur and tab[4][longueur]==couleur and tab[5][longueur]==couleur :
     alignement_de_4_pions=False
   return alignement_de_4_pions
-def f_verifdiago(hauteur,longueur,alignement_de_4_pions,couleur,tab):
+
+def f_verifdiago(hauteur,longueur,alignement_de_4_pions,couleur,tab):  #cette foncyion verifie toute les diagonales de la grille, en se deplacant de la gauche vers la droite
   if tab[2][0]==couleur and  tab[3][1]==couleur and  tab[4][2]==couleur and  tab[5][3]==couleur :
     alignement_de_4_pions=False
   elif tab[1][0]==couleur and  tab[2][1]==couleur and  tab[3][2]==couleur and  tab[5][3]==couleur :
@@ -37,7 +39,8 @@ def f_verifdiago(hauteur,longueur,alignement_de_4_pions,couleur,tab):
   elif tab[0][3]==couleur and  tab[1][4]==couleur and  tab[2][5]==couleur and  tab[3][6]==couleur :
     alignement_de_4_pions=False 
   return alignement_de_4_pions
-def f_verifautrediago(hauteur,longueur,alignement_de_4_pions,couleur,tab):
+
+def f_verifautrediago(hauteur,longueur,alignement_de_4_pions,couleur,tab): #cette foncyion verifie toute les diagonales de la grille, en se deplacant de la droite vers la gauche
   if tab[0][3]==couleur and  tab[1][2]==couleur and  tab[2][1]==couleur and  tab[3][0]==couleur :
     alignement_de_4_pions=False
   elif tab[0][4]==couleur and  tab[1][3]==couleur and  tab[2][2]==couleur and  tab[3][1]==couleur :
@@ -63,6 +66,7 @@ def f_verifautrediago(hauteur,longueur,alignement_de_4_pions,couleur,tab):
   elif tab[2][6]==couleur and  tab[3][5]==couleur and  tab[4][4]==couleur and  tab[5][3]==couleur :
     alignement_de_4_pions=False
   return alignement_de_4_pions
+
 def puissance4():
   tab=[['.','.','.','.','.','.','.'],
      ['.','.','.','.','.','.','.'],
@@ -70,7 +74,7 @@ def puissance4():
      ['.','.','.','.','.','.','.'],
      ['.','.','.','.','.','.','.'],
      ['.','.','.','.','.','.','.']]
-  alignement_de_4_pions=True
+  alignement_de_4_pions=True #quand false cela veut dire que 4 pions on ete alligne 
   while alignement_de_4_pions:
     a=int(input("Dans quelle colonne voulez vous placer un pions. "))#pour le joueur 1 
     if a>7:
